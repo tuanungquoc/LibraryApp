aa2b77ae9470ce7aefbe7efc7ce44c4b7976adc8
Vimmi Swami <vimmi.swami@outlook.com>
2017-12-03 13:58:54 -0800

    adding cloud mongodb

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://localhost:27017/test'
+    //'database': 'mongodb://localhost:27017/test'
+    'database': 'mongodb://libraryapp:libraryapp277pwd@ds125716.mlab.com:25716/library277'
 };
